91f9a065cdcad29244c3faa978a83e4b9056bcb1
Nicolas <nicolas-donald.laurent@laposte.net>
2018-08-02 23:15:24 +0200

    Benchmark vector vs new array

--- Src/SpacePirateTools.pro
+++ Src/SpacePirateTools.pro
@@ -292,3 +292,12 @@ CONFIG(debug, debug|release): LIBS +=   -lsfml-graphics-s-d \
                                         -lvorbis \
                                         -logg
 
+
+
+# remove possible other optimization flags
+QMAKE_CXXFLAGS_RELEASE -= -O
+QMAKE_CXXFLAGS_RELEASE -= -O1
+QMAKE_CXXFLAGS_RELEASE -= -O2
+
+# add the desired -O3 if not present
+QMAKE_CXXFLAGS_RELEASE *= -O3
--- Src/main.cpp
+++ Src/main.cpp
@@ -19,3 +19,93 @@ int main(int argc, char *argv[])
 
     return returnValue;
 }
+
+/*
+#include <iostream>
+#include <vector>
+#include <string>
+#include <chrono>
+
+struct Point
+{
+    float x, y, z;
+    Point& operator+=(const Point & other)
+    {
+        x += other.x;
+        y += other.y;
+        z += other.z;
+        return *this;
+    }
+};
+
+void execVector(unsigned int size)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+
+    std::vector<Point> v{size};
+
+    auto step1 = std::chrono::high_resolution_clock::now();
+
+    for(unsigned int i(0) ; i < size ; i++)
+        v[i] = {i, i+1, i+2};
+
+    auto step2 = std::chrono::high_resolution_clock::now();
+
+    Point p{0, 0, 0};
+    for(unsigned int i(0) ; i < size ; i++)
+        p += v[i];
+
+    auto step3 = std::chrono::high_resolution_clock::now();
+
+    std::cout << "---- Vector ---- size " << size << std::endl;
+    std::cout << "Allocation " << std::chrono::duration<double>(step1-start).count() << "s" << std::endl;
+    std::cout << "Fill " << std::chrono::duration<double>(step2-step1).count() << "s" << std::endl;
+    std::cout << "Use " << std::chrono::duration<double>(step3-step2).count() << "s" << std::endl;
+    std::cout << "Total " << std::chrono::duration<double>(step3-start).count() << "s" << std::endl;
+    std::cout << p.x << " " << p.y << " " << p.z << std::endl << std::endl;
+}
+
+void execTab(unsigned int size)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+
+    Point* v = new Point[size];
+
+    auto step1 = std::chrono::high_resolution_clock::now();
+
+    for(unsigned int i(0) ; i < size ; i++)
+        v[i] = {i, i+1, i+2};
+
+    auto step2 = std::chrono::high_resolution_clock::now();
+
+    Point p{0, 0, 0};
+    for(unsigned int i(0) ; i < size ; i++)
+        p += v[i];
+
+    auto step3 = std::chrono::high_resolution_clock::now();
+
+    std::cout << "---- Array ---- size " << size << std::endl;
+    std::cout << "Allocation " << std::chrono::duration<double>(step1-start).count() << "s" << std::endl;
+    std::cout << "Fill " << std::chrono::duration<double>(step2-step1).count() << "s" << std::endl;
+    std::cout << "Use " << std::chrono::duration<double>(step3-step2).count() << "s" << std::endl;
+    std::cout << "Total " << std::chrono::duration<double>(step3-start).count() << "s" << std::endl;
+    std::cout << p.x << " " << p.y << " " << p.z << std::endl << std::endl;
+
+    delete[] v;
+}
+
+int main()
+{
+    execVector(100000);
+    execTab(100000);
+
+    execVector(1000000);
+    execTab(1000000);
+
+    execVector(10000000);
+    execTab(10000000);
+
+    execVector(100000000);
+    execTab(100000000);
+}
+*/
